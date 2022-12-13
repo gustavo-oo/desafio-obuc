79e28753d5f8d3a4ab2800366201b022939a3119
Gustavo <gustavopchaves@hotmail.com>
2022-12-13 08:35:04 -0300

    ✨ Adicionado hover para botões e botão secundário

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const router = createBrowserRouter(routes);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div id="dialog-div" />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
